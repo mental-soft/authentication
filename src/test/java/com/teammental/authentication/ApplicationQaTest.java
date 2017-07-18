c384a565a3ba9f9a5620cab0745371fb40414cdc
Coşkun Deniz <coskundeniz1989@gmail.com>
2017-07-18 21:36:12 +0300

    ATHE-9 changed test

--- src/test/java/com/teammental/authentication/ApplicationQaTest.java
+++ src/test/java/com/teammental/authentication/ApplicationQaTest.java
@@ -2,34 +2,35 @@ package com.teammental.authentication;
 
 import static org.junit.Assert.assertFalse;
 
-import liquibase.integration.spring.SpringLiquibase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Qa ortamı için hazırlanan test sınıfı.
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-@ActiveProfiles("qa")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(initializers = ConfigFileApplicationContextInitializer.class)
+@ActiveProfiles(value = {"qa", "prod"})
 public class ApplicationQaTest {
 
-  @Autowired
-  SpringLiquibase liquibase;
+  @Value(value = "${liquibase.drop-first}")
+  String isDropFirst;
 
   /**
    * Liquibase qa ortamında drop yapmaması için test ediliyor.
+   *
    * @throws Exception exception
    */
   @Test
   public void checkDropFirst() throws Exception {
-    assertFalse(liquibase.isDropFirst());
+    if (!isDropFirst.equals("${liquibase.drop-first}")) {
+      assertFalse(Boolean.getBoolean(isDropFirst));
+    }
   }
 
 }
